b4d50288b51e0cc3d612b5c74d141669424ca0d0
mimi <kuni2097@gmail.com>
2022-09-19 06:35:52 +0900

    로그 찍어보기

--- app/src/main/java/com/miminerica/kotlinbasic01/MainActivity.kt
+++ app/src/main/java/com/miminerica/kotlinbasic01/MainActivity.kt
@@ -2,11 +2,19 @@ package com.miminerica.kotlinbasic01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn.setOnClickListener {
+            Log.d("메인화면로그","클릭 버튼 눌림")
+
+
+        }
 
 
 
